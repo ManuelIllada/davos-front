2480ac69f91a942aa12fc216212f2abb73d61946
ManuelIllada <ignacio.illada@fing.edu.uy>
2023-08-19 23:31:16 -0300

    Casi terminado

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="p-1 bg-black">
+      <div className="flex justify-center items-center h-16">
+        <img className="h-full " src="/image/logodavos.png" alt="logo" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
