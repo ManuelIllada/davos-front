92f0ad78fc5e7ea187d2077b957f9e3195455625
ManuelIllada <ignacio.illada@fing.edu.uy>
2023-08-19 23:54:48 -0300

    Cambios finales antes de que cambien el back (finalizado)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="p-1 bg-black">
+    <nav className="p-1 bg-black sticky top-0">
       <div className="flex justify-center items-center h-16">
         <img className="h-full " src="/image/logodavos.png" alt="logo" />
       </div>
